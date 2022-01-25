b1053373c51bfa46ae5ce557a3ceabd277186499
Agasthi98 <sankalana98@gmail.com>
2022-01-25 22:51:06 +0530

    dd

--- crud2/src/Components/InputForm.js
+++ crud2/src/Components/InputForm.js
@@ -1,8 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import ListView from "./ListView";
+
+const getDatafromLS = () => {
+  const data = localStorage.getItem("tasks");
+  if (data) {
+    return JSON.parse(data);
+  } else {
+    return [];
+  }
+};
 
 const InputForm = () => {
+  const [taskList, setTaskList] = useState(getDatafromLS());
+  const [taskName, setTaskName] = useState("");
+
+  const AddTask = (e) => {
+    e.preventDefault();
+
+    const task = {
+      taskName,
+    };
+    setTaskList([...taskList, task]);
+    setTaskName("");
+    console.log(task);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+  }, [taskList]);
+
   return (
-      <h1>Hii</h1>
+    <>
+    <div className="container">
+      <div>
+        <form onSubmit={AddTask}>
+          <input
+            type="text"
+            value={taskName}
+            onChange={(e) => setTaskName(e.target.value)}
+            required
+          />
+          <button type="submit">Submit </button>
+        </form>
+      </div>
+
+      <div className="container">
+        <center>
+        {taskList.length > 0 && (
+          <>
+            <div>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <ListView tasks = {taskList} />
+                </tbody>
+              </table>
+            </div>
+            <button>
+              Remove All
+            </button>
+          </>
+        )}
+        {taskList.length < 1 && <h5>No books are added yet</h5>}
+        </center>
+      </div>
+      </div>
+    </>
   );
 };
 
---
+++ crud2/src/Components/ListView.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ListView = () => {
+  return (
+      <h1>gg</h1>
+  );
+};
+
+export default ListView;
